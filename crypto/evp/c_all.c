34b409966ada60d9203776d67fb160f405f0630c
steve <steve>
1999-03-28 23:17:13 +0000

    Yet more PKCS#12 integration: add lots of files under crypto/pkcs12 and add them to the build environment.

--- crypto/evp/c_all.c
+++ crypto/evp/c_all.c
@@ -1,5 +1,5 @@
 /* crypto/evp/c_all.c */
-/* Copyright (C) 1995-1997 Eric Young (eay@cryptsoft.com)
+/* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
  * This package is an SSL implementation written
@@ -59,6 +59,7 @@
 #include <stdio.h>
 #include "cryptlib.h"
 #include "evp.h"
+#include "pkcs12.h"
 #include "objects.h"
 
 void SSLeay_add_all_algorithms()
@@ -79,16 +80,16 @@ void SSLeay_add_all_ciphers()
 	EVP_add_cipher(EVP_des_ede3_ofb());
 
 	EVP_add_cipher(EVP_desx_cbc());
-	EVP_add_alias(SN_desx_cbc,"DESX");
-	EVP_add_alias(SN_desx_cbc,"desx");
+	EVP_add_cipher_alias(SN_desx_cbc,"DESX");
+	EVP_add_cipher_alias(SN_desx_cbc,"desx");
 
 	EVP_add_cipher(EVP_des_cbc());
-	EVP_add_alias(SN_des_cbc,"DES");
-	EVP_add_alias(SN_des_cbc,"des");
+	EVP_add_cipher_alias(SN_des_cbc,"DES");
+	EVP_add_cipher_alias(SN_des_cbc,"des");
 	EVP_add_cipher(EVP_des_ede_cbc());
 	EVP_add_cipher(EVP_des_ede3_cbc());
-	EVP_add_alias(SN_des_ede3_cbc,"DES3");
-	EVP_add_alias(SN_des_ede3_cbc,"des3");
+	EVP_add_cipher_alias(SN_des_ede3_cbc,"DES3");
+	EVP_add_cipher_alias(SN_des_ede3_cbc,"des3");
 
 	EVP_add_cipher(EVP_des_ecb());
 	EVP_add_cipher(EVP_des_ede());
@@ -97,6 +98,7 @@ void SSLeay_add_all_ciphers()
 
 #ifndef NO_RC4
 	EVP_add_cipher(EVP_rc4());
+	EVP_add_cipher(EVP_rc4_40());
 #endif
 
 #ifndef NO_IDEA
@@ -104,8 +106,8 @@ void SSLeay_add_all_ciphers()
 	EVP_add_cipher(EVP_idea_cfb());
 	EVP_add_cipher(EVP_idea_ofb());
 	EVP_add_cipher(EVP_idea_cbc());
-	EVP_add_alias(SN_idea_cbc,"IDEA");
-	EVP_add_alias(SN_idea_cbc,"idea");
+	EVP_add_cipher_alias(SN_idea_cbc,"IDEA");
+	EVP_add_cipher_alias(SN_idea_cbc,"idea");
 #endif
 
 #ifndef NO_RC2
@@ -113,8 +115,10 @@ void SSLeay_add_all_ciphers()
 	EVP_add_cipher(EVP_rc2_cfb());
 	EVP_add_cipher(EVP_rc2_ofb());
 	EVP_add_cipher(EVP_rc2_cbc());
-	EVP_add_alias(SN_rc2_cbc,"RC2");
-	EVP_add_alias(SN_rc2_cbc,"rc2");
+	EVP_add_cipher(EVP_rc2_40_cbc());
+	EVP_add_cipher(EVP_rc2_64_cbc());
+	EVP_add_cipher_alias(SN_rc2_cbc,"RC2");
+	EVP_add_cipher_alias(SN_rc2_cbc,"rc2");
 #endif
 
 #ifndef NO_BLOWFISH
@@ -122,9 +126,29 @@ void SSLeay_add_all_ciphers()
 	EVP_add_cipher(EVP_bf_cfb());
 	EVP_add_cipher(EVP_bf_ofb());
 	EVP_add_cipher(EVP_bf_cbc());
-	EVP_add_alias(SN_bf_cbc,"BF");
-	EVP_add_alias(SN_bf_cbc,"bf");
-	EVP_add_alias(SN_bf_cbc,"blowfish");
+	EVP_add_cipher_alias(SN_bf_cbc,"BF");
+	EVP_add_cipher_alias(SN_bf_cbc,"bf");
+	EVP_add_cipher_alias(SN_bf_cbc,"blowfish");
+#endif
+
+#ifndef NO_CAST
+	EVP_add_cipher(EVP_cast5_ecb());
+	EVP_add_cipher(EVP_cast5_cfb());
+	EVP_add_cipher(EVP_cast5_ofb());
+	EVP_add_cipher(EVP_cast5_cbc());
+	EVP_add_cipher_alias(SN_cast5_cbc,"CAST");
+	EVP_add_cipher_alias(SN_cast5_cbc,"cast");
+	EVP_add_cipher_alias(SN_cast5_cbc,"CAST-cbc");
+	EVP_add_cipher_alias(SN_cast5_cbc,"cast-cbc");
+#endif
+
+#ifndef NO_RC5
+	EVP_add_cipher(EVP_rc5_32_12_16_ecb());
+	EVP_add_cipher(EVP_rc5_32_12_16_cfb());
+	EVP_add_cipher(EVP_rc5_32_12_16_ofb());
+	EVP_add_cipher(EVP_rc5_32_12_16_cbc());
+	EVP_add_cipher_alias(SN_rc5_cbc,"rc5");
+	EVP_add_cipher_alias(SN_rc5_cbc,"RC5");
 #endif
 	}
 
@@ -134,8 +158,10 @@ void SSLeay_add_all_digests()
 #ifndef NO_MD2
 	EVP_add_digest(EVP_md2());
 #endif
-#ifndef NO_MD2
+#ifndef NO_MD5
 	EVP_add_digest(EVP_md5());
+	EVP_add_digest_alias(SN_md5,"ssl2-md5");
+	EVP_add_digest_alias(SN_md5,"ssl3-md5");
 #endif
 #ifndef NO_SHA
 	EVP_add_digest(EVP_sha());
@@ -145,11 +171,22 @@ void SSLeay_add_all_digests()
 #endif
 #ifndef NO_SHA1
 	EVP_add_digest(EVP_sha1());
+	EVP_add_digest_alias(SN_sha1,"ssl3-sha1");
+	EVP_add_digest_alias(SN_sha1WithRSAEncryption,SN_sha1WithRSA);
 #ifndef NO_DSA
 	EVP_add_digest(EVP_dss1());
+	EVP_add_digest_alias(SN_dsaWithSHA1,SN_dsaWithSHA1_2);
+	EVP_add_digest_alias(SN_dsaWithSHA1,"DSS1");
+	EVP_add_digest_alias(SN_dsaWithSHA1,"dss1");
 #endif
 #endif
 #if !defined(NO_MDC2) && !defined(NO_DES)
 	EVP_add_digest(EVP_mdc2());
 #endif
+#ifndef NO_RMD160
+	EVP_add_digest(EVP_ripemd160());
+	EVP_add_digest_alias(SN_ripemd160,"ripemd");
+	EVP_add_digest_alias(SN_ripemd160,"rmd160");
+#endif
+	PKCS12_PBE_add();
 	}
